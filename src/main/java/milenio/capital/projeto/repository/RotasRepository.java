95148a4def409d1283e2deca028f82d7d1e46d2f
edvaldojr <edvaldoluizj@gmail.com>
2022-04-29 10:54:10 -0300

    Repository onde vamos acessar as entidades do banco de dados

---
+++ src/main/java/milenio/capital/projeto/repository/RotasRepository.java
@@ -0,0 +1,7 @@
+package milenio.capital.projeto.repository;
+
+import milenio.capital.projeto.entity.Rotas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RotasRepository extends JpaRepository<Rotas, Long> {
+}
