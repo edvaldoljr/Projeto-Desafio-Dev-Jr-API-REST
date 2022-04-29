45503e98aebb5d3029e3b5462df23b549168340d
edvaldojr <edvaldoluizj@gmail.com>
2022-04-29 11:42:07 -0300

    Controller responsável pelo processamento das requisições e por gerar respostas

---
+++ src/main/java/milenio/capital/projeto/controller/RotasController.java
@@ -0,0 +1,37 @@
+package milenio.capital.projeto.controller;
+
+import milenio.capital.projeto.entity.Rotas;
+import milenio.capital.projeto.service.RotasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rotas")
+public class RotasController {
+
+    @Autowired
+    private RotasService rotasService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Rotas salvar(@RequestBody Rotas rotas){
+        return rotasService.salvar(rotas);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Rotas> listaCliente(){
+        return rotasService.listaRotas();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Rotas buscarId(@PathVariable("id") Long id){
+        return rotasService.buscarId(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Id não encontrado na base de dados"));
+    }
+}
